efd4d275fd268666632836c3eeccfc72870c0771
Milad Abrahamson <33893565+3i14d@users.noreply.github.com>
2025-03-08 02:09:47 +0100

    Update csts.js

--- csts.js
+++ csts.js
@@ -15,6 +15,7 @@ Das Verständnis dieser unterschiedlichen Risikoeinstellungen ist in der Spielth
   <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0wIT89B1NOxu4RR5bygcqw?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/5uBqrp5dDwZePTFbQijpzr?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
   <div class="timestamp">1741378989</div>
+    <div class="timestamp">1741396164</div>
   `,
   String.raw`
     <p>
